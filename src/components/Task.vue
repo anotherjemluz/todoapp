f67f62893a73e09983a0bf6066a3e12e3793a2eb
Jemima <contatojemima@gmail.com>
2020-08-14 16:57:22 -0300

    tasks

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,5 +1,13 @@
 <template lang="pug">
-  
+  transition(name="fade")
+    .task(v-if="!task.deleted")
+      input(:id="id" type="checkbox" v-model="task.done")
+      label(:for="id") {{ task.title }}
+
+      transition(name="fade")
+        span.task_delete(v-show="task.done" @click="deleteTask")
+          i.fa.fa-trash
+
 </template>
 
 <script>
